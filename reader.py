74c0c6c1cf2b53b5c3bec9b4aa9e8ba8ce64a3b9
theEndurance-del <tomarshubhanshu@protonmail.ch>
2022-01-20 22:27:14 +0530

    Windows support added to `dev` branch

--- reader.py
+++ reader.py
@@ -1,4 +1,5 @@
 from rich import console, traceback
+from platform import platform
 Console = console.Console()
 
 def args(value: list, pos: int) -> str:
@@ -159,7 +160,11 @@ class Reader:
 
         return ''.join(splitted)
     
-    def read(self, prompt: str = ">") -> str:
+    def read(self, prompt: str = '>') -> str:
+        if 'Windows' in platform(): return self._read_windows(prompt)
+        else: return self._read_unix(prompt)
+    
+    def _read_unix(self, prompt) -> str:
         req_line_buff_len: int = 0
         getch = _Getch()
         string: str = ''
@@ -177,18 +182,86 @@ class Reader:
             if ord(char) == 4: raise EOFError()
             if ord(char) == 3: raise KeyboardInterrupt()
             if ord(char) == 127:
-                req_line_buff_len = len(string)
                 try:
-                    string_break = list(string)
-                    string_break.pop()
-                    string=''.join(string_break)
-                    del string_break
+                    string = string[:-1:]
                 except IndexError:
                     string=''
                     pass
                 finally:
                     continue
-            if char.isprintable():
+            if len(str(char)) == 1 and str(char).isprintable():
                 string+=char
         print()
+        return string
+    
+    def _read_windows(self, prompt) -> str:
+        getch = _Getch()
+        string: str = ''
+        current = len(self.history_buff)-1
+        temp_string_buff: str = ''
+        while True:
+            last_buff: str = args(self.history_buff, -1)
+            print('\033[2K\033[1G', end='')
+            Console.print(f'\r{prompt} {self._prettify(string)}', end="")
+            b_char = getch()
+            try:
+                char = bytes.decode(b_char, 'utf-8')
+            except UnicodeDecodeError:
+                if b_char == b'\xe0':
+                    # print('directional_initiator')
+                    direction = bytes.decode(getch(), 'utf-8')
+                    # print(direction)
+                    if direction == 'H':
+                        ## UP
+                        if string and not temp_string_buff: temp_string_buff = string
+                        if current >= 0 and current < len(self.history_buff):
+                            string=self.history_buff[current]
+                            current-=1
+                        else:
+                            if temp_string_buff:
+                                string = temp_string_buff
+                                temp_string_buff = ''
+                                current=len(self.history_buff)-1
+                            else:
+                                string = last_buff
+                                current = len(self.history_buff)-1
+                    if direction == 'P':
+                        ## DOWN 
+                        if current < len(self.history_buff):
+                            current+=1
+                            string=args(self.history_buff, current)
+
+                        else:
+                            if temp_string_buff:
+                                string=temp_string_buff
+                                temp_string_buff=''
+                            else:
+                                string=''
+                            current=len(self.history_buff)-1
+                    if direction == 'K': pass #print('LT')
+                    if direction == 'M': pass #print('RT')
+                else: print(b_char)
+                continue
+            if char == '\r':
+                if args(self.history_buff, -1) != string:
+                    self.history_buff.append(string)
+                else:
+                    pass
+                break
+            if ord(char) == 8:
+                try:
+                    string = string[:-1:]
+                except IndexError:
+                    string = ''
+                    continue
+            if ord(char) == 4: print('^D');raise EOFError
+            if ord(char) == 3:
+                if string:
+                    print('^C')
+                    string = ''
+                else: raise KeyboardInterrupt
+            # print(ord(char))
+            if char.isprintable(): string+=char
+        print()
+        print(self.history_buff)
         return string
