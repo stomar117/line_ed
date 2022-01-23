e6572647bafdd32742fe9c92f6d680206c1370e1
theEndurance-del <tomarshubhanshu@protonmail.ch>
2022-01-23 16:52:22 +0530

    Implemented navigation, ctrl+backspace, del

--- reader.py
+++ reader.py
@@ -169,15 +169,18 @@ class Reader:
         else: return self._read_unix(prompt)
     
     def _read_unix(self, prompt) -> str:
-        req_line_buff_len: int = 0
         getch = _Getch()
-        string: str = ''
+        string_bc: str = ''
+        string_ac: str = ''
         char: str = ''
         current: int = len(self.history_buff)-1
+        temp_string_buff=''
         while True:
+            string = string_bc+string_ac
             print('\033[2K\033[1G', end='')
             # self._prettify(string)
-            Console.print(f'\r{prompt} {self._prettify(string)}', end="")
+            Console.print(f'\r{prompt} {self._prettify(string_bc+string_ac)}', end="\r")
+            Console.print(f'\r{prompt} {self._prettify(string_bc)}', end="")
             char = getch()
             # print(ord(char))
             if char == '\r':
@@ -185,16 +188,102 @@ class Reader:
                 break
             if ord(char) == 4: raise EOFError()
             if ord(char) == 3: raise KeyboardInterrupt()
+            if ord(char) == 27:
+                initializer = getch()
+                if ord(initializer) == 91:
+                    direction = getch()
+                    if direction == 'A':
+                        #UP
+                        if string and not temp_string_buff: temp_string_buff = string
+                        if current >= 0 and current < len(self.history_buff):
+                            string_bc=self.history_buff[current]
+                            current-=1
+                        else:
+                            if temp_string_buff:
+                                string_bc = temp_string_buff
+                                temp_string_buff = ''
+                                current=len(self.history_buff)-1
+                            else:
+                                string_bc = last_buff
+                                current = len(self.history_buff)-1
+                        string_ac=''
+                    elif direction == 'B':
+                        ## DOWN 
+                        if current < len(self.history_buff):
+                            current+=1
+                            string_bc=args(self.history_buff, current)
+
+                        else:
+                            if temp_string_buff:
+                                string_bc=temp_string_buff
+                                temp_string_buff=''
+                            else:
+                                string_bc=''
+                            current=len(self.history_buff)-1
+                        string_ac=''
+                    elif direction == 'C':
+                        ## RIGHT
+                        if string_ac:
+                            string_bc+=string_ac[0]
+                            if len(string_ac) > 1:
+                                string_ac=string_ac[1::]
+                            else:
+                                string_ac=''
+                        # print('RT')
+                    elif direction == 'D':
+                        ## LEFT
+                        if string_bc:
+                            temp_char = string_bc[-1]
+                            string_bc=string_bc[:-1:]
+                            temp_char+=string_ac
+                            string_ac=temp_char
+                            del temp_char
+
+                    elif ord(direction) == 72:
+                        ## HOME
+                        string_ac=string_bc+string_ac
+                        string_bc=''
+
+                    elif ord(direction) == 70:
+                        string_bc+=string_ac
+                        string_ac=''
+
+                    elif ord(direction) == 51:
+                        new_unnamed_identifier = getch()
+                        if ord(new_unnamed_identifier) == 126:
+                            if len(string_ac) > 1:
+                                string_ac=string_ac[1::]
+                            else:
+                                string_ac=''
             if ord(char) == 127:
                 try:
-                    string = string[:-1:]
+                    string_bc = string_bc[:-1:]
                 except IndexError:
-                    string=''
+                    string_bc=''
                     pass
                 finally:
                     continue
+            if ord(char) == 8:
+                if string_bc:
+                    strlist: list = string.split(' ')
+                    if strlist[-1]:
+                        strlist.pop()
+                        string_bc=' '.join(strlist)
+                    else:
+                        for x in range(len(strlist)-1,-1, -1):
+                            if strlist[x]:
+                                strlist.pop()
+                                break
+                            else:
+                                strlist.pop()
+                        
+                        string_bc=' '.join(strlist)
+                    del strlist
+                continue
+
             if len(str(char)) == 1 and str(char).isprintable():
-                string+=char
+                string_bc+=char
+        string = string_bc+string_ac
         print()
         return string
     
