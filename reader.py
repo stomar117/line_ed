616a476f94d4a89eb6223b2fc2e0a7cac7ee78ca
theEndurance-del <tomarshubhanshu@protonmail.ch>
2022-01-23 23:01:10 +0530

    Organized code

--- reader.py
+++ reader.py
@@ -2,6 +2,8 @@ from rich import console, traceback
 from platform import platform
 from os import path
 import _config as config
+from util.splitters import Splitters
+from util.getch import Getch
 Console = console.Console()
 
 def args(value: list, pos: int) -> str:
@@ -9,127 +11,6 @@ def args(value: list, pos: int) -> str:
     except Exception: return ''
 
 traceback.install()
-
-class Splitters:
-    @staticmethod
-    def bracket(string: str, bropen: str = '('):
-        openbr: str = '({[<'
-        closebr: str = ')}]>'
-        if bropen in openbr: brclose = closebr[openbr.find(bropen)]
-        else:
-            # print("Not a valid bracket...")
-            return None
-        str_container: list = []
-        form_string: str = ''
-        check: int = 0
-        nbuff: int = 0
-        for ch in string:
-            if check == 1: form_string+=ch
-            if ch == bropen:
-                check = 1
-                nbuff+=1
-            if ch == brclose:
-                nbuff-=1
-                if nbuff == 0: check = 0
-                if nbuff < 0:
-                    # print("Extra closing bracket found. Qutting...")
-                    check = 1
-                    break
-            if check == 0:
-                if form_string:
-                    uneeded = list(form_string)
-                    uneeded.pop()
-                    form_string=''.join(uneeded)
-                    del(uneeded)
-                    str_container.append(form_string)
-                    form_string = ''
-        if check == 0: return str_container
-        else:
-            # if nbuff >= 0: print("Extra opening bracket found. Quitting...")
-            return []
-
-    @staticmethod
-    def quote(string: str, delimiter: str = ' ') -> list:
-        if delimiter.isalnum(): raise ValueError('delimiter cannot be an alpha-numeric character')
-        form_string: str = ''
-        str_container: list[str] = []
-        quote_string: str = ''
-        check: int = 0
-        previous_state: int = 0
-        for ch in string:
-            previous_state = check
-            if ch == '\'':
-                if check == 2: pass
-                elif check == 1:
-                    quote_string+=ch
-                    check = 0
-                elif check == 0:
-                    quote_string+=ch
-                    check = 1
-            if ch == '"':
-                if check == 1: pass
-                elif check == 2:
-                    quote_string+=ch
-                    check = 0
-                elif check == 0:
-                    quote_string+=ch
-                    check = 2
-            if check == 0:
-                if previous_state != check:
-                    str_container.append(quote_string)
-                    quote_string = ''
-                elif ch != delimiter and previous_state == check: form_string+=ch
-                else:
-                    # if form_string:
-                        str_container.append(form_string+ch)
-                        form_string=''
-            else:
-                if form_string: str_container.append(form_string); form_string=''
-                # if check == 2 and ch == '"': pass
-                # elif check == 1 and ch == '\'': pass
-                else:
-                    if len(quote_string.strip()) == 1:
-                        if ch == '\'' and quote_string == '\'': pass
-                        elif ch == '"' and quote_string == '"': pass
-                        else: quote_string+=ch
-
-                    else: quote_string+=ch
-        if form_string: str_container.append(form_string)
-        if quote_string: str_container.append(quote_string)
-        return str_container
-
-class _Getch:
-    """Gets a single character from standard input.  Does not echo to the screen."""
-    def __init__(self):
-        try:
-            self.impl = _GetchWindows()
-        except ImportError:
-            self.impl = _GetchUnix()
-
-    def __call__(self): return self.impl()
-
-class _GetchUnix:
-    def __init__(self):
-        import tty, sys
-
-    def __call__(self):
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-
-class _GetchWindows:
-    def __init__(self) -> None:
-        import msvcrt
-
-    def __call__(self):
-        import msvcrt
-        return msvcrt.getch()
 
 class Reader:
     def __init__(self, valid_cmd_list: list, _history_buff: list = [])-> None:
@@ -195,7 +76,7 @@ class Reader:
         else: return self._read_unix(prompt)
     
     def _read_unix(self, prompt) -> str:
-        getch = _Getch()
+        getch = Getch()
         string_bc: str = ''
         string_ac: str = ''
         char: str = ''
@@ -329,7 +210,7 @@ class Reader:
         return string
 
     def _read_windows(self, prompt) -> str:
-        getch = _Getch()
+        getch = Getch()
         string: str = ''
         current = len(self.history_buff)-1
         temp_string_buff: str = ''
